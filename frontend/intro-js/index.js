9f06d545a5b2513ab48360ddcf2c6aa798bab15d
jsonvm <jheyson.v.m1@gmail.com>
2020-02-06 07:39:04 -0500

    introduccion a js

---
+++ frontend/intro-js/index.js
@@ -0,0 +1,24 @@
+
+// console
+
+/**
+ * Comentario en bloque
+ */
+console.log("hola mundo")
+console.info("Info")
+console.error("Error")
+console.warn("Alerta")
+
+let nombre = "Jheyson"
+const edad = 50
+var color = "blanco"
+
+if (edad > 46){
+    let nombre = "Jheysanax"
+    var color = "negro"
+    console.log("Dentro de if", nombre, color);
+    
+}
+
+console.log(color);
+console.log(nombre);
